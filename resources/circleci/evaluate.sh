ff3e2b458ff3f704966631aef321606253777fe6
psychobolt <psychobolt@gmail.com>
2017-03-08 09:06:53 -0800

    use tox command argument instead of bash scripts

--- resources/circleci/evaluate.sh
+++
@@ -1,6 +0,0 @@
-rm -rf ./qbotio-resources
-git clone https://github.com/techcats/qbotio-resources.git ./qbotio-resources
-cd ./qbotio-resources/
-pip install -r requirements.txt
-bash ./eval_suite.sh
-cd ..
